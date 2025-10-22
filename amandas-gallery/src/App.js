13506892b284c2eed611e4a7a069118a5cb39014
Pammade <pam.madee@gmail.com>
2025-10-21 22:07:10 -0400

    aaaa

---
+++ amandas-gallery/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Top from './pages/Top';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Top />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ amandas-gallery/src/pages/Top.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../pretty/Top.css';
+
+import '../assets/smiskiiii.png'
+
+function Top() {
+  return (
+    <div className="top">
+    <div className="hello">
+    <div className="title">
+        <img src= {require('../assets/smiskiiii.png')} alt="smiski" className="smiski-image" />
+        <h1> amanda zhu .ᐟ </h1>
+    </div>
+    <div className="bullets">
+        <p> ⊹ uix/ux/graphic/logo design</p>
+        <p> ⊹ growing queen's ux club (co-founded last year)</p>                
+        <p> ⊹ studying biomedical computing @ queen's u</p> 
+    </div>
+    </div>
+    <div className="navbar">
+        <p>all</p>
+        <p>work</p>
+        <p>fun-work</p>
+        <p>contact</p>
+        <p>resume</p>
+    </div>
+    </div>
+    
+  );
+}
+
+export default Top;
